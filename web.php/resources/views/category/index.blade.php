795fb9b2da39d85daecbf6c3220b5bfd119f0f03
Camila Alves <91034827+CammyFerreira@users.noreply.github.com>
2022-11-21 23:09:36 -0300

    deletando a pasta do pi

--- web.php/resources/views/category/index.blade.php
+++
@@ -1,8 +0,0 @@
-<h1>Esta é a página que retorna as categorias</h1>
-
-@foreach($categories as $category)
-    <li>{{$category->name}}
-    <a href="{{ route('category.edit', $category->id) }}" >Editar</a>
-    <a href="{{ route('category.destroy', $category->id) }}" >Deletar</a>
-    </li>
-@endforeach
